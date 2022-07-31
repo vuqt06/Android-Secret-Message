5bf39f3c9cd7bb25dc8a44fc41033a7f7fdce6c9
vuqt06 <vuqt06@gmail.com>
2022-07-30 21:00:26 -0400

    Add navigation host to activity_main.xml

--- app/src/main/java/com/codewithvu/secretmessage/WelcomeFragment.kt
+++ app/src/main/java/com/codewithvu/secretmessage/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +23,12 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val startButton = view.findViewById<Button>(R.id.start)
+
+        startButton.setOnClickListener {
+
+        }
+        return view
     }
 }
