8db019d4e1f46ad80bcab9868d1ffaf188c94d56
vuqt06 <vuqt06@gmail.com>
2022-07-31 09:18:21 -0400

    Add navigation controller to navigate from WelcomeFragment to MessageFragment

--- app/src/main/java/com/codewithvu/secretmessage/WelcomeFragment.kt
+++ app/src/main/java/com/codewithvu/secretmessage/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +28,7 @@ class WelcomeFragment : Fragment() {
         val startButton = view.findViewById<Button>(R.id.start)
 
         startButton.setOnClickListener {
-
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_messageFragment)
         }
         return view
     }
