1485977cee933684324ad4cf434e5c86fb4d5d19
vuqt06 <vuqt06@gmail.com>
2022-08-02 12:10:47 -0400

    Import SafeArgs to the project - Pass the message from MessageFragment to EncryptFragment - Encrypt the message and display it

--- app/src/main/java/com/codewithvu/secretmessage/EncryptFragment.kt
+++ app/src/main/java/com/codewithvu/secretmessage/EncryptFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class EncryptFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val encryptedMessage = view.findViewById<TextView>(R.id.encrypted_text)
+        encryptedMessage.text = message.reversed()
+        return view
     }
 }
--- app/src/main/java/com/codewithvu/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/codewithvu/secretmessage/MessageFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
@@ -14,12 +15,14 @@ class MessageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_message, container, false)
         val nextButton = view.findViewById<Button>(R.id.next)
+        val messageView = view.findViewById<EditText>(R.id.message)
+        val message = messageView.text.toString()
 
+        val action = MessageFragmentDirections.actionMessageFragmentToEncryptFragment(message)
         nextButton.setOnClickListener {
-            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+            view.findNavController().navigate(action)
         }
 
         return view
